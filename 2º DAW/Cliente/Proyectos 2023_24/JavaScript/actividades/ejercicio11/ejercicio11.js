e854cd64a605c457fdae60176ba62013729a08c7
Ambrosio0304 <ambrosio0304@gmail.com>
2023-09-29 11:38:30 +0200

    ejercicio11

--- 2º DAW/Cliente/Proyectos 2023_24/JavaScript/actividades/ejercicio11/ejercicio11.js
+++ 2º DAW/Cliente/Proyectos 2023_24/JavaScript/actividades/ejercicio11/ejercicio11.js
@@ -1,29 +1,14 @@
 /**
  * @author: Ambrosio Iglesias Prieto
- * @description:Escribe una función llamada dividirFragmento que tome un array arr y un número entero
-tamano . La función debe dividir el array en fragmentos de tamaño tamano y devolver un nuevo
-array con los Fragmentos.
+ * @description:Define una función llamada mapearNumeros que toma dos argumentos: un array arr y una
+función funcion . La función debe aplicar la función proporcionada a cada elemento del array y
+devolver un nuevo array con los resultados de la función aplicada a cada elemento.
  */
-const dividirFragmento = (arr, tamano) => {
-  
-  if (tamano <= 0 || arr.length === 0) {
-    return [];
-  }
 
-  
-  const fragmentos = Math.ceil(arr.length / tamano);
+const mapearNumeros = (arr,funcion) => arr.map(funcion);
 
-  
-  const resultado = [];
-  for (let i = 0; i < fragmentos; i++) {
-    const inicio = i * tamano;
-    const fin = inicio + tamano;
-    resultado.push(arr.slice(inicio, fin));
-  }
+let numeros = [1, 2, 3, 4, 5];
+let cuadrados = mapearNumeros(numeros, numero => numero * numero);
+console.log(cuadrados); // Imprime: [1, 4, 9, 16, 25]
 
-  return resultado;
-};
-
-
-const miArray = [1, 2, 3, 4, 5, 6, 7];
 
