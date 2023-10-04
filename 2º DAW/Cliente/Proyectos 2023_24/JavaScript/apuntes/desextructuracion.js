113f4c6eeeb5ff9df7e86106c636041045af0534
Ambrosio0304 <ambrosio0304@gmail.com>
2023-10-04 11:54:38 +0200

    entornos

---
+++ 2º DAW/Cliente/Proyectos 2023_24/JavaScript/apuntes/desextructuracion.js
@@ -0,0 +1,13 @@
+const user = {
+    id : 890,
+    name : "Luis",
+    geo : {
+        coordx : 45,
+        coordy : 90,
+    },
+    books : ["libro1","libro2","libro3","libro4"],
+    basura : "menuda m....",
+    carnet : true,
+}
+
+const { id, geo, aaa } = user;
